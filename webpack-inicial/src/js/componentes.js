e62b19246d4d860563779ef671312d2fc8359941
joshuaRiv <brayanrivera10@gmail.com>
2022-10-07 15:33:48 -0500

    webpack configurado

--- webpack-inicial/src/js/componentes.js
+++ webpack-inicial/src/js/componentes.js
@@ -1,7 +1,7 @@
 import '../css/componentes.css';
 // import webpacklogo from '../assets/img/webpack-logo.png'
 
-export const saludar = (nombre) =>{
+export const saludar = (nombre = 'Sin nombre') =>{
     console.log('Creando etiqueta h1');
 
     const h1 = document.createElement( 'h1' );
--- webpack-inicial/webpack.config.js
+++ webpack-inicial/webpack.config.js
@@ -1,11 +1,13 @@
-const HtmlWebpack = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpack    = require('html-webpack-plugin')
+const MiniCssExtract = require('mini-css-extract-plugin');
+const CopyPlugin     = require("copy-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
+    
+    mode: "development",
 
     output: {
-        clean: true,
+        clean: true
     },
 
     module: {
@@ -14,45 +16,43 @@ module.exports = {
                 test: /\.html$/,
                 loader: 'html-loader',
                 options: {
-                    sources: false,
+                    sources: false
                 }
             },
             {
                 test: /\.css$/,
                 exclude: /styles.css$/,
-                use: [
-                    'style-loader', 'css-loader'
-                ]
+                use: [ 'style-loader', 'css-loader']
             },
             {
                 test: /styles.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
+                use: [ MiniCssExtract.loader, 'css-loader' ]
             },
             {
                 test: /\.(png|jpe?g|gif)$/,
-                loader: 'file-loader',
+                loader: 'file-loader'
             }
         ]
     },
 
     optimization: {},
 
-    // arregla servidor no actualizando los archivos 
-    devServer: { watchFiles: ["src/*.html", "src/*.css"], hot: true, },
-
     plugins: [
         new HtmlWebpack({
-            title: 'Mi webpack App',
-            // filename: 'index.html'
+            title: 'Mi Webpack App',
+            // filename: 'index.html',
             template: './src/index.html'
         }),
-        new MiniCssExtractPlugin({
-            // fullhash ayuda a que los navegadores no mantengan el cache del archivo
+        
+        new MiniCssExtract({
             filename: '[name].css',
             ignoreOrder: false
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets/', to: 'assets/' }
+            ]
         })
-    ],
+    ]
 }
